dfe82e06a5a42a6f945e86f7d830a89111bf992a
Ryan Teves <rteves@ucsc.edu>
2017-09-15 10:39:10 -0700

    grading problem done

---
+++ algorithms/implementation/grading.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class grading {
+
+    static int[] solve(int[] grades){
+	for (int i=0; i < grades.length; i++){
+		int multiple = grades[i];
+		while(multiple%5 != 0){
+			multiple +=1;
+		}
+		if(multiple - grades[i] < 3 && grades[i] >=38)
+			grades[i] = multiple;
+	}
+	return grades;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] grades = new int[n];
+        for(int grades_i=0; grades_i < n; grades_i++){
+            grades[grades_i] = in.nextInt();
+        }
+        int[] result = solve(grades);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
