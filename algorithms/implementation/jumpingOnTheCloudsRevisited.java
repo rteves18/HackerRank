ba8cc3cb0dc424b770b8247900e57f3d88f99a31
Ryan Teves <rteves@ucsc.edu>
2017-10-17 15:20:49 -0700

    added another solved problem

--- algorithms/implementation/jumpingOnTheCloudsRevisited.java
+++ algorithms/implementation/jumpingOnTheCloudsRevisited.java
@@ -10,8 +10,14 @@ public class jumpingOnTheCloudsRevisited {
         int n = in.nextInt();
         int k = in.nextInt();
         int c[] = new int[n];
+	int energy = 100;
         for(int c_i=0; c_i < n; c_i++){
-            c[c_i] = in.nextInt();
+            c[c_i] = in.nextInt(); 
         }
+	for(int i = 0; i < n; i+=k){
+		energy -= 1;			
+		if(c[i] == 1) energy -= 2;
+	}
+	System.out.println(energy);
     }
 }
