2845dcfc46d26edd54771157c6bb5695a3e5c4d9
Ryan Teves <rteves@ucsc.edu>
2017-11-06 14:08:40 -0800

    solved cut the sticks problem

--- algorithms/implementation/cutTheStick.java
+++ algorithms/implementation/cutTheStick.java
@@ -5,19 +5,27 @@ import java.math.*;
 import java.util.regex.*;
 
 public class cutTheStick {
+
     public static void main(String args[] ) throws Exception {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
-	Set<Integer> set = new HashSet<Integer>();
-	try{
-        	for(int i=0; i < n; i++){
-			set.add(in.nextInt());
-		} 
-	} catch(Exception e) {}
-	
-	for(Integer x : set){
-		System.out.println(x);
+        for(int arr_i=0; arr_i < n; arr_i++){
+            arr[arr_i] = in.nextInt();	
+	}
+	Arrays.sort(arr);
+	System.out.println(n);
+	int sticks = n;
+	int count = 0;
+	for(int i=0; i < n-1; i++){
+		if(arr[i] == arr[i+1]){
+			count += 1;		
+		} else if(arr[i] != arr[i+1]){
+			count += 1;
+			sticks = sticks - count;
+			System.out.println(sticks);
+			count = 0;
+		}	
 	}
     }
 }
