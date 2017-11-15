0580a645461ab7d4591e57ae81fdbf5399105a73
Ryan Teves <rteves@ucsc.edu>
2017-11-14 17:16:26 -0800

    added solved problem

---
+++ algorithms/implementation/equalizeTheArray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class equalizeTheArray {
+    public static void main(String args[] ) throws Exception {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	int[] arr = new int[n];
+	int[] freq = new int[100];
+	int maxCount = 0;
+	int maxNum = 0;
+	for(int i = 0; i < n; i++){
+		arr[i] = in.nextInt();
+		freq[arr[i]]++;
+		//max num and curent num the same
+		if(maxNum == arr[i]){
+			maxCount++;
+		//current count is more than maxCount
+		} else if(freq[arr[i]] > maxCount){
+			maxNum = arr[i];
+			maxCount = freq[arr[i]];
+		}
+	}
+	System.out.println(n - maxCount);
+	
+    }
+}
